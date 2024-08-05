fa7f632a2f0020453657acd29a6c80ce49a912d9
Poosit <poosit.pims@gmail.com>
2024-08-05 14:58:16 +0700

    change resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,8 +17,8 @@ function Header({ darkMode, setDarkMode }) {
           <li>
             <a
               className="bg-gradient-to-r from-blue-600 to-teal-300 text-white px-4 py-2 rounded-md ml-8 hover:from-red-600 hover:to-yellow-200"
-              href="/poosit-resume.pdf"
-              download="poosit-resume.pdf"
+              href="/poosit_resume.pdf"
+              download="poosit_resume.pdf"
             >
               Resume
             </a>
